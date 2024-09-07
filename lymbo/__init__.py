3088d315d894dc9496cc9f5b49bf01b9a04ffaab
cle-b <cle@tictac.pm>
2024-09-07 07:53:54 +0200

    unused

--- lymbo/__init__.py
+++ lymbo/__init__.py
@@ -1,4 +1,3 @@
-import logging
 from lymbo.cm import args
 from lymbo.cm import params
 from lymbo.cm import test
@@ -7,5 +6,3 @@ from lymbo.cm import test
 __all__ = ["args", "params", "test"]
 
 __version__ = "0.0.1"
-
-logger = logging.getLogger("lymbo")
