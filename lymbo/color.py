1d2ec4560058c5fdc530a3d56085a91b917f6f34
cle-b <cle@tictac.pm>
2024-09-21 14:01:19 +0200

    no color for windows shell (#22)

--- lymbo/color.py
+++ lymbo/color.py
@@ -1,4 +1,12 @@
-RED = "\033[31m"
-GREEN = "\033[32m"
-YELLOW = "\033[33m"
-RESET = "\033[0m"
+import os
+
+if os.name == "nt":
+    RED = ""
+    GREEN = ""
+    YELLOW = ""
+    RESET = ""
+else:
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    RESET = "\033[0m"
