b378ab1d8101662f7893de9e89d482c2f1f514b8
cle-b <cle@tictac.pm>
2024-09-29 20:46:56 +0200

    format

--- lymbo/config.py
+++ lymbo/config.py
@@ -8,7 +8,9 @@ from lymbo.log import LogLevel
 
 def parse_args() -> argparse.Namespace:
 
-    parser = argparse.ArgumentParser(description="A test runner designed for large test suites.")
+    parser = argparse.ArgumentParser(
+        description="A test runner designed for large test suites."
+    )
 
     parser.add_argument(
         "paths",
